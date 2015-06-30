465b7d2178390003566c1fec46c065204f7b2282
BrianBeeler <brianlbeeler@gmail.com>
2015-06-30 16:50:45 -0500

    Refactored code

--- server/request-handler.js
+++ server/request-handler.js
@@ -1,16 +1,3 @@
-/*************************************************************
-
-You should implement your request handler function in this file.
-
-requestHandler is already getting passed to http.createServer()
-in basic-server.js, but it won't work as is.
-
-You'll have to figure out a way to export this function from
-this file and include it in basic-server.js so that it actually works.
-
-*Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
-
-**************************************************************/
 var resultObj = {};
 resultObj.results = [];
 
@@ -21,53 +8,51 @@ exports.requestHandler = function(request, response) {
   var statusCode;
   var body = "";
 
-  var defaultCorsHeaders = {
-  "access-control-allow-origin": "*",
+  var headers = {
+  "access-control-allow-origin":  "*",
   "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
   "access-control-allow-headers": "content-type, accept",
-  "access-control-max-age": 10 // Seconds.
+  "access-control-max-age": 10, // Seconds.
+  "Content-Type": "application/json"
   };
 
-  var headers = defaultCorsHeaders;
+  var completeHead = function(response, statusCode) {
+    statusCode = statusCode || 404;
+    response.writeHead(statusCode, headers);
+  }
 
-  headers['Content-Type'] = "application/json";
-  //Check to see if url is correct
+  var actions = {
+    'GET': function () {
+      completeHead(response,200);
+      response.end(JSON.stringify(resultObj));
+    },
+    'POST': function () {
+       completeHead(response, 201);
+       request.on('data', function(chunk) {
+         body += chunk;
+       });
+       request.on('end', function() {
+         var parsedBody = JSON.parse(body);
+         resultObj.results.push(parsedBody);
+         response.end(JSON.stringify({success: true}));
+       })
+    },
+    'ERROR': function () {
+      completeHead(response, 404);
+      response.end(JSON.stringify({success: false}));
+    }
+  }
+
   if (request.url !== '/classes/messages') {
-    statusCode = 404;
-    response.writeHead(statusCode, headers);
-    response.end(JSON.stringify({success: false}));
+    actions.ERROR();
   }
-  // The outgoing status.
   if(request.method === 'POST') {
-     statusCode = 201;
-     response.writeHead(statusCode, headers);
-     request.on('data', function(chunk) {
-       body += chunk;
-     });
-
-     request.on('end', function() {
-       var parsedBody = JSON.parse(body);
-       resultObj.results.push(parsedBody);
-       response.end(JSON.stringify({success: true}));
-     });
+    actions.POST();
   }
-
   else if(request.method === 'GET') {
-    statusCode = 200;
-    response.writeHead(statusCode, headers);
-    response.end(JSON.stringify(resultObj));
+    actions.GET()
   }
 };
 
-
-// These headers will allow Cross-Origin Resource Sharing (CORS).
-// This code allows this server to talk to websites that
-// are on different domains, for instance, your chat client.
-//
-// Your chat client is running from a url like file://your/chat/client/index.html,
-// which is considered a different domain.
-//
-// Another way to get around this restriction is to serve you chat
-// client from this domain by setting up static file serving.
 
 
